2ae8e2669acdb0858fcfa9f7b95cad3ceb646942
Knight <beijingtexan@gmail.com>
2016-07-16 13:36:23 -0500

    editing header logo

--- public/wp-content/themes/goodz-magazine/header.php
+++ public/wp-content/themes/goodz-magazine/header.php
@@ -44,7 +44,7 @@
 			    </div>
 		    	<div class="navbar-header col-sm-4 center-block">
 		      		<a href="<?php echo home_url(); ?>">
-		            	<img class="site-logo-link img-responsive" id="header-logo" src="/wp-content/colab-logo.png">
+		            	<img class="img-responsive center-block" id="header-logo" src="/wp-content/colab-logo.png">
 		        	</a>
 		    	</div>
 			    <div class="navbar-header center-block">
