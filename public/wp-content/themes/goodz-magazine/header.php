25bbb84c2d241506ce0227ca079177abad7f60a8
Knight <beijingtexan@gmail.com>
2016-07-16 13:15:39 -0500

    editing header logo

--- public/wp-content/themes/goodz-magazine/header.php
+++ public/wp-content/themes/goodz-magazine/header.php
@@ -27,7 +27,7 @@
 	  <div class="container-fluid">
 	    	<!-- Brand and toggle get grouped for better mobile display -->
 		  	<div class="row">
-			    <div class="navbar-header">
+			    <div class="col-sm-2">
 			      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 			        <span class="sr-only">Toggle navigation</span>
 			        <span class="icon-bar"></span>
