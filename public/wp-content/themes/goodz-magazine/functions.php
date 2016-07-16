2d5d44a79ea23d778609db4c6aa82d9fdd3640b6
Knight <beijingtexan@gmail.com>
2016-07-16 14:00:05 -0500

    editing menus

--- public/wp-content/themes/goodz-magazine/functions.php
+++ public/wp-content/themes/goodz-magazine/functions.php
@@ -57,6 +57,7 @@ function goodz_magazine_setup() {
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
 		'primary' => esc_html__( 'Primary Menu', 'goodz-magazine' ),
+		'business' => esc_html__( 'Business', 'goodz-magazine' ),
 	) );
 
 	/*
--- public/wp-content/themes/goodz-magazine/header.php
+++ public/wp-content/themes/goodz-magazine/header.php
@@ -27,7 +27,7 @@
 	    <div class="container-fluid col-sm-12">
 	    	<!-- Brand and toggle get grouped for better mobile display -->
 		  	<div class="row">
-		    	<div class="col-sm-4 center-block text-capitalize">
+		    	<div class="col-sm-4 center-block">
 		        <?php
 		            wp_nav_menu( array(
 		                'menu'              => 'primary',
@@ -55,7 +55,7 @@
 			        <span class="icon-bar"></span>
 			      </button>
 			    </div>
-		    	<div class="col-sm-4 center-block text-capitalize">
+		    	<div class="col-sm-4 center-block">
 		        <?php
 		            wp_nav_menu( array(
 		                'menu'              => 'primary',
