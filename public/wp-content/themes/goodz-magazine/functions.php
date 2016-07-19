567ad7d9a1b33c94036b73fad614d1865e43e87d
Knight <beijingtexan@gmail.com>
2016-07-19 10:50:01 -0500

    editing nav menu dropdown

--- public/wp-content/themes/goodz-magazine/functions.php
+++ public/wp-content/themes/goodz-magazine/functions.php
@@ -275,7 +275,7 @@ require_once get_template_directory() . '/inc/importer/init.php';
 add_action('wp_enqueue_styles', 'my_styles');
 function my_styles(){
     wp_enqueue_style('twitter', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
-	wp_enqueue_script( 'bootstrap-jquery', get_stylesheet_directory_uri() . 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' );
+	wp_enqueue_script( 'bootstrap-jquery', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js' );
     wp_enqueue_style('theme-css', get_stylesheet_uri(), 'twitter');
 }
 
